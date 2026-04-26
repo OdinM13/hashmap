826f2c5fe903d783312d2996e861239ba59b59ec
OdinM13 <markodinius@gmail.com>
2026-04-26 16:01:21 +0200

    WIP: Added function

---
+++ LinkedList.mjs
@@ -0,0 +1,145 @@
+export class LinkedList;
+
+class LinkedList {
+  constructor() {
+    this.header = null;
+  }
+
+  append(value) {
+    // adds a new node containing value to the end of the list.
+    const newNode = new Node(value);
+    if (this.header === null) {
+      this.header = newNode;
+      newNode.next = null;
+      return;
+    }
+    let tmp = this.header;
+    while (tmp.next !== null) {
+      tmp = tmp.next;
+    } 
+    tmp.next = newNode;
+    newNode.next = null; 
+  }
+
+  prepend(value) {
+    // adds a new node containing value to the start of the list.
+    const newNode = new Node(value);
+    newNode.next = this.header;
+    this.header = newNode;
+  }
+
+  size() {
+    // returns the total number of nodes in the list.
+    let tmp = this.header;
+    let counter = 0;
+    while (tmp !== null) {
+      tmp = tmp.next;
+      counter += 1;
+    }
+    return counter;
+  }
+
+  head() {
+    // should return the value of the first node in the list. If the list is empty, it should return undefined.
+    if (this.header === null) {
+      return undefined;
+    } else {
+      return this.header.data;
+    }
+  }
+
+  tail() {
+    // should return the value of the final node in the list. If the list is empty, it should return undefined.
+    if (this.header === null) {
+      return undefined;
+    } else {
+      let tmp = this.header;
+      while (tmp.next !== null) {
+        tmp = tmp.next;
+      }
+      return tmp.data;
+    }
+  }
+
+  at(index) {
+    // should return the value of the node at the given index. If there’s no node at the given index, it should return undefined.
+    const sizeOfLinkedList = this.size();
+    if (index > sizeOfLinkedList - 1) {
+      return undefined;
+    }
+    if (this.header === null) {
+      return undefined;
+    } else {
+      let tmp = this.header;
+      let value = "";
+      for (let i = 0; i <= index; i++) {
+        value = tmp.data;
+        tmp = tmp.next;
+      }
+      return value;
+    }
+  }
+
+  pop() {
+    // should remove the header node from the list and return its value. If it’s used on an empty list, it should just return undefined.
+    if (this.header === null) {
+      return undefined;
+    } else {
+      const data = this.header.data;
+      this.header = this.header.next;
+      return data;
+    }
+  }
+
+  contains(value) {
+    // returns true if the passed in value is in the list and otherwise returns false.
+    if (this.header === null) {
+      return false;
+    }
+    let tmp = this.header;
+    while (tmp !== null) {
+      if (tmp.data === value) {
+        return true;
+      }
+      tmp = tmp.next;
+    }
+    return false;
+  }
+
+  findIndex(value) {
+    // returns the index of the node containing the given value. If the value can’t be found in the list, it should return -1. 
+    // If more than one node has a value matching the given value, it should return the index of the first node with the matching value.
+    let tmp = this.header;
+    let counter = 0;
+    while (tmp !== null) {
+      if (tmp.data === value) {
+        return counter;
+      }
+      tmp = tmp.next;
+      counter += 1;
+    }
+    return -1;
+  }
+
+  toString() {
+    // represents your LinkedList objects as strings, so you can print them out and preview them in the console. 
+    // If the list is empty, it should return an empty string. The format should be: ( value ) -> ( value ) -> ( value ) -> null.
+    let tmp = this.header;
+    let string = '';
+    while (tmp !== null ) {
+      string += `( ${tmp.data} ) -> `
+      tmp = tmp.next;
+    }
+    string += 'null';
+    return string;
+  }
+}
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+
--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import { LinkedList } from './LinkedList.mjs';
+
 class HashMap {
   constructor () {
     this.loadFactor = 0.75;
@@ -19,49 +21,116 @@ class HashMap {
     // takes two arguments: the first is a key, and the second is a value that is assigned to this key.     
     // If a key already exists, then the old value is overwritten, and we can say that we update the key’s value 
     const hashCode = this.hash(key);
-    this.bucket[hashCode] = {key: key, value: value};
+    if (this.has(key)) {
+      this.bucket[hashCode] = {key: key, value: value};
+    }
+    if (this.bucket[hashCode] !== null) {
+      // Create LinkedList
+    }
     console.log(hashCode);
-    console.log(this.bucket);
+    this.expandCapacity();
   }
 
   get(key) {
     // takes one argument as a key and returns the value that is assigned to this key. If a key is not found, return null.
+    if (this.has(key) === false) {
+      return null;
+    }
+    const hashCode = this.hash(key);
+    const returnValue = this.bucket[hashCode].value;
+    return returnValue;
   }
 
   has(key) {
     // takes a key as an argument and returns true or false based on whether or not the key is in the hash map.
+    const hashCode = this.hash(key);
+    if (hashCode < 0 || hashCode > this.capacity) {
+      return false;
+    }
+    if (this.bucket[hashCode].key === key) {
+      return true;
+    }
+   return false; 
   }
 
   remove(key) {
     // takes a key as an argument. 
     // If the given key is in the hash map, it should remove the entry with that key and return true. If the key isn’t in the hash map, it should return false.
+    if (this.has(key) === false) {
+      return false;
+    }
+    const hashCode = this.hash(key);
+    this.bucket[hashCode] = null;
+    return true;
   }
 
   length() {
     // returns the number of stored keys in the hash map.
+    let count = 0;
+    this.bucket.forEach(e => {
+      if (e !== null) {
+        count ++;
+      }
+    });
+    return count;
+  }
+
+  currentLoadFactor() {
+    const currentLoadFactor = this.length() / this.capacity;
+    return currentLoadFactor;
+  }
+
+  expandCapacity() {
+    if (this.currentLoadFactor() > this.loadFactor) {
+      this.capacity *= 2;
+      const tmpBucket = this.bucket;
+      this.bucket = new Array(this.capacity).fill(null);
+      tmpBucket.forEach(e => {
+        if (e !== null) {
+          set(e.key, e.value);
+        }
+      })
+    }
   }
 
   clear() {
     // removes all entries in the hash map
+    this.bucket.fill(null);
   }
 
   keys() {
     // returns an array containing all the keys inside the hash map
+    let keysArray = [];
+    this.bucket.forEach(e => {
+      if (e !== null) {
+        keysArray.push(e.key);
+      }
+    });
+    return keysArray;
   }
 
   values() {
     // returns an array containing all the values
+    let valuesArray = [];
+    this.bucket.forEach(e => {
+      if (e !== null) {
+        valuesArray.push(e.value);
+      }
+    });
+    return valuesArray;
   }
 
   entries() {
     // returns an array that contains each key, value pair.
+    let entriesArray = [];
+    this.bucket.forEach(e => {
+      if (e !== null) {
+        entriesArray.push(e);
+      }
+    });
+    return entriesArray;
   }
 }
-
-
-// if (index < 0 || index >= buckets.length) {
-//   throw new Error("Trying to access index out of bounds");
-// }
 
 const test = new HashMap();
 
@@ -77,5 +146,29 @@ test.set('ice cream', 'white');
 test.set('jacket', 'blue');
 test.set('kite', 'pink');
 test.set('lion', 'golden');
+console.log(test.bucket);
+console.log(test.length());
+console.log(`Current Load Factor: ` + test.currentLoadFactor());
+test.set('moon', 'silver');
+console.log(test.bucket);
+console.log(`Current Load Factor: ` + test.currentLoadFactor());
+console.log(test.bucket);
 
+console.log(`Key lion has value:`, test.get('lion'));
+console.log(`Key rhino has value: ` + test.get('rhino'));
+
+console.log(`Has key lion: ` + test.has('lion'));
+console.log(`Has key rhino: ` + test.has('rhino'));
+
+console.log(`Remove key lion: ` + test.remove('lion'));
+console.log(`Remove key rhino: ` + test.remove('rhino'));
+console.log(test.bucket);
+console.log(test.length());
+
+console.log(test.entries());
+console.log(test.values());
+console.log(test.keys());
+
+test.clear();
+console.log(test.bucket);
 
